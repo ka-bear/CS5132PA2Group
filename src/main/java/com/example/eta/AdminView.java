ddd567b55a227ea729305b85afbd62f5d79563b6
cakular <99185144+cakular@users.noreply.github.com>
2023-10-05 22:59:11 +0800

    added admin and user view

---
+++ src/main/java/com/example/eta/AdminView.java
@@ -0,0 +1,10 @@
+package com.example.eta;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminView {
+
+    @FXML
+    private AnchorPane adminPane;
+}
---
+++ src/main/java/com/example/eta/UserView.java
@@ -0,0 +1,10 @@
+package com.example.eta;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class UserView {
+
+    @FXML
+    private AnchorPane userPane;
+}
