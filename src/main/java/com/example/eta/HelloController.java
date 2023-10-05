5f7227d82bfa5ddcb0bdb343aa8cf30138c0796f
cakular <99185144+cakular@users.noreply.github.com>
2023-10-06 01:13:04 +0800

    tabs works

--- src/main/java/com/example/eta/HelloController.java
+++ src/main/java/com/example/eta/HelloController.java
@@ -1,14 +1,56 @@
 package com.example.eta;
 
+import io.github.palexdev.mfxresources.fonts.IconsProviders;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
+import javafx.scene.paint.Color;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private StackPane centerStackPane;
+
+
+    private AnchorPane adminPane;
+    private AnchorPane userPane;
+
+    @FXML
+    private MFXRectangleToggleNode userButton;
+
+
+    public void initialize() throws IOException {
+        adminPane = FXMLLoader.load(getClass().getResource("admin-view.fxml"));
+        userPane = FXMLLoader.load(getClass().getResource("user-view.fxml"));
+        centerStackPane.getChildren().addAll(adminPane, userPane);
+        adminPane.setVisible(false);
+
+
+
     }
+
+    @FXML
+    private void userbuttonclick() {
+        adminPane.setVisible(false);
+        userPane.setVisible(true);
+        System.out.println("user");
+    }
+
+    @FXML
+    private void adminbuttonclick() {
+        adminPane.setVisible(true);
+        userPane.setVisible(false);
+        System.out.println("admin");
+    }
+
+
+
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.eta {
     requires javafx.controls;
     requires javafx.fxml;
+    requires MaterialFX;
 
 
     opens com.example.eta to javafx.fxml;
